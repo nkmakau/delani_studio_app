8ef3a1866bc3c16cbc65786d3ef460e134780fc0
Noah Makau <noshmak@gmail.com>
2020-06-30 17:53:31 +0300

    add server code different steps

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-let http = require('http');
-
+/*let http = require('http');
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-type": "text/plain"});
     response.write("Hello World");
     response.end();
-}).listen(8000);
+}).listen(8000);*/
+
+
+/*let http = require('http');
+
+function onRequest (request, response){
+    response.writeHead(200, {"Content-type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);*/
+
+let http = require('http');
+
+function onRequest (request, response){
+    console.log("Request recieved!")
+    response.writeHead(200, {"Content-type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);
+
+console.log("Server has started")
