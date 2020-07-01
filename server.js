7a0e68d9feae391e4e69385858e1c478e9f4409a
Noah Makau <noshmak@gmail.com>
2020-07-01 08:37:14 +0300

    add url and route

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ function onRequest (request, response){
 
 http.createServer(onRequest).listen(8000);*/
 
-let http = require('http');
+/*let http = require('http');
 
 function onRequest (request, response){
     console.log("Request recieved!")
@@ -27,4 +27,32 @@ function onRequest (request, response){
 
 http.createServer(onRequest).listen(8000);
 
-console.log("Server has started")
+console.log("Server has started")*/
+
+//Wednesday
+//defination shd be done for http and url
+let http = require('http');
+let url = require('url')
+//A function to wrap our server functionality so that we can export it
+let start = function (route) {
+
+    function onRequest(request, response) {
+        //Extracting the pathname from the url requested
+        let pathname = url.parse(request.url).pathname
+        console.log("Request for " + pathname + " has been received.")
+        
+        //passing the pathname as a parameter to the route function
+        route(pathname);
+
+        response.writeHead(200, { "Content-type": "text/plain" });
+        response.write("Hello World");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8000);
+
+    console.log("Server has started")
+}
+exports.start = start;
+
+
