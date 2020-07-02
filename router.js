949efdb9a50cf0ed30a3774b2663dcc580977fd4
Noah Makau <noshmak@gmail.com>
2020-07-02 12:28:07 +0300

    Correct eror on code hindering css.

--- router.js
+++ router.js
@@ -6,21 +6,21 @@ function route(handle, pathname, response) {
     //check if the request url is a function. since we mapped our expected request urls to a function in requerst handlers
     if (typeof handle[pathname] === 'function') {
         handle[pathname](response)
-    } else if (pathname.match("./css$")) {
+    } else if (pathname.match("\.css$")) {
         let cssPath = path.join(__dirname, 'public', pathname);
         let cssStream = fs.createReadStream(cssPath);
         response.writeHead(200, { "Content-Type": "text/css" })
         cssStream.pipe(response);
     } else if (pathname.match('\.png$') || pathname.match('\.jpg$')) {
-        let imagePath = path.join(_dirname, 'public', pathname);
+        let imagePath = path.join(__dirname, 'public', pathname);
         let imageStream = fs.createReadStream(imagePath);
         response.writeHead(200, { "Conte-Type": "text/png" });
         imageStream.pipe(response);
     } else if (pathname.match('\..js$')) {
-        let jsPath = path.join(_dirname, 'public', pathname);
+        let jsPath = path.join(__dirname, 'public', pathname);
         let jsStream = fs.createReadStream(jsPath)
         response.writeHead(200, { "Content-Type": "text/js" })
-        jsStream.pipe(response)
+        jsStream.pipe(response);
     } else {
         console.log("No request handler found for " + pathname);
         response.writeHead(404, { "Content-Type": "text/html" });
