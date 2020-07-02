b0e24a1f4a8bf2c3e7af35142691e5078aa86ca2
Noah Makau <noshmak@gmail.com>
2020-07-02 12:16:47 +0300

    add the path module

--- router.js
+++ router.js
@@ -1,11 +1,29 @@
+let path = require('path');
+let fs = require('fs');
+
 function route(handle, pathname, response) {
     console.log("About to route a request for" + pathname);
     //check if the request url is a function. since we mapped our expected request urls to a function in requerst handlers
     if (typeof handle[pathname] === 'function') {
         handle[pathname](response)
+    } else if (pathname.match("./css$")) {
+        let cssPath = path.join(__dirname, 'public', pathname);
+        let cssStream = fs.createReadStream(cssPath);
+        response.writeHead(200, { "Content-Type": "text/css" })
+        cssStream.pipe(response);
+    } else if (pathname.match('\.png$') || pathname.match('\.jpg$')) {
+        let imagePath = path.join(_dirname, 'public', pathname);
+        let imageStream = fs.createReadStream(imagePath);
+        response.writeHead(200, { "Conte-Type": "text/png" });
+        imageStream.pipe(response);
+    } else if (pathname.match('\..js$')) {
+        let jsPath = path.join(_dirname, 'public', pathname);
+        let jsStream = fs.createReadStream(jsPath)
+        response.writeHead(200, { "Content-Type": "text/js" })
+        jsStream.pipe(response)
     } else {
         console.log("No request handler found for " + pathname);
-        response.writeHead(404, { "Content-Type": "text/plain" });
+        response.writeHead(404, { "Content-Type": "text/html" });
         response.write("404 Not found");
         response.end();
     }
